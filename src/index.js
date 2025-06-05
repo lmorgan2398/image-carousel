c5e9e5b276cd16f5a1f98020cfab290f9ec3390f
lmorgan2398 <lmorgan2398@gmail.com>
2025-06-05 21:42:59 +0000

    Modularize function to apply style to multiple slideshows
    Any created slideshow elements with appropriate CSS classes
    will now render with proper styling and functionality, allows
    for multiple slideshows per page with independtly stored
    variables

--- src/index.js
+++ src/index.js
@@ -1,78 +1,82 @@
 import "./styles.css";
 
-let slideIndex = 0;
-let autoSlideInterval;
-let selectors = document.querySelectorAll(".selector");
+let slideshows = document.querySelectorAll(".slideshow");
+slideshows.forEach((slideshow) => {
+  let slideIndex = 0;
+  let autoSlideInterval;
+  let selectors = slideshow.querySelectorAll(".selector");
 
-showSlide(slideIndex);
-startAutoSlide();
-
-// Next/prev control
-function plusSlide(n) {
-  showSlide((slideIndex += n));
-}
-
-function showSlide(n) {
-  let slides = document.querySelectorAll(".slide");
-  if (n > slides.length - 1) {
-    n = 0;
-  } else if (n < 0) {
-    n = slides.length - 1;
-  }
-  slides.forEach((slide) => {
-    if (slide.classList.contains("active")) {
-      slide.classList.remove("active");
-    }
-  });
-  selectors.forEach((selector) => {
-    if (selector.classList.contains("active")){
-      selector.classList.remove("active");
-    }
-  });
-  slideIndex = n;
-  console.log(slideIndex);
-  let currentSlide = slides[slideIndex];
-  let currentSelector = selectors[slideIndex];
-  currentSlide.classList.add("active");
-  currentSelector.classList.add("active");
-}
-
-function setSlide(n) {
-  slideIndex = n;
-  console.log(slideIndex);
   showSlide(slideIndex);
-  console.log(slideIndex);
-}
+  startAutoSlide();
 
-let prev = document.querySelector(".prev");
-prev.addEventListener("click", () => {
-  plusSlide(-1);
-});
+  // Next/prev control
+  function plusSlide(n) {
+    showSlide((slideIndex += n));
+  }
 
-let next = document.querySelector(".next");
-next.addEventListener("click", () => {
-  plusSlide(1);
-});
+  function showSlide(n) {
+    let slides = slideshow.querySelectorAll(".slide");
+    if (n > slides.length - 1) {
+      n = 0;
+    } else if (n < 0) {
+      n = slides.length - 1;
+    }
+    slides.forEach((slide) => {
+      if (slide.classList.contains("active")) {
+        slide.classList.remove("active");
+      }
+    });
+    selectors.forEach((selector) => {
+      if (selector.classList.contains("active")){
+        selector.classList.remove("active");
+      }
+    });
+    slideIndex = n;
+    console.log(slideIndex);
+    let currentSlide = slides[slideIndex];
+    let currentSelector = selectors[slideIndex];
+    currentSlide.classList.add("active");
+    currentSelector.classList.add("active");
+  }
 
-function startAutoSlide() {
-  console.log("start");
-  autoSlideInterval = setInterval(() => {
+  function setSlide(n) {
+    slideIndex = n;
+    console.log(slideIndex);
+    showSlide(slideIndex);
+    console.log(slideIndex);
+  }
+
+  let prev = slideshow.querySelector(".prev");
+  prev.addEventListener("click", () => {
+    plusSlide(-1);
+  });
+
+  let next = slideshow.querySelector(".next");
+  next.addEventListener("click", () => {
     plusSlide(1);
-  }, 5000);
-}
+  });
 
-function stopAutoSlide() {
-  console.log("stop");
-  clearInterval(autoSlideInterval);
-}
+  function startAutoSlide() {
+    if (autoSlideInterval) return;
+    console.log("start");
+    autoSlideInterval = setInterval(() => {
+      plusSlide(1);
+    }, 5000);
+  }
 
-let slideshow = document.querySelector(".slideshow");
-slideshow.addEventListener("mouseenter", stopAutoSlide);
-slideshow.addEventListener("mouseleave", startAutoSlide);
+  function stopAutoSlide() {
+    console.log("stop");
+    clearInterval(autoSlideInterval);
+    autoSlideInterval = null;
+  }
 
-selectors.forEach((selector) => {
-  selector.addEventListener('click', () => {
-    let index = Number(selector.dataset.index);
-    setSlide(index);
+  slideshow.addEventListener("mouseenter", stopAutoSlide);
+  slideshow.addEventListener("mouseleave", startAutoSlide);
+
+  selectors.forEach((selector) => {
+    selector.addEventListener('click', () => {
+      let index = Number(selector.dataset.index);
+      setSlide(index);
+    })
   })
 })
